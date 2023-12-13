c0719f8b261fdec395ffa2a2566e707f1cf9d743
alex <voronalost@gmail.com>
2023-12-13 15:12:43 +0300

    Добавил метод changedArray, добавил переменную для условия размера элементов нового массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿string[] inputnumber()
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+string[] inputnumber()
 {
     bool isNumber = false;
     Console.WriteLine($"Введите количество элементов в массиве:");
@@ -20,12 +23,31 @@
     return arr;
 }
 
-void printarray(string[] arr)
+string[] changedArray(string[] array)
 {
-    Console.WriteLine($"Вы ввели следующий массив:");
-    Console.WriteLine($"'{String.Join("' ", arr)}'");
+    int p = 0;
+    int amount = 3;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= amount) p +=1;
+    string[] newArray = new string[p];
+    for (int j = 0, k = 0; j < array.Length; j++)
+        if (array[j].Length <= amount)
+        {
+            newArray[k] = array[j]; 
+            k +=1;
+        } 
+    return newArray;
 }
 
+void printarray(string[] arr, string text)
+{
+    Console.WriteLine($"{text}");
+    Console.WriteLine($"\"{String.Join("\" \"", arr)}\"");
+}
+
+
 string[] array = inputnumber();
-printarray(array);
+printarray(array, "Вы ввели массив:");
+string[] newArray = changedArray(array);
+printarray(newArray, "Новый массив:");
 
